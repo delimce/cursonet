26b362a887d69a383abe94157c271080b12690e0
Luis De Lima <delimce@gmail.com>
2018-07-24 22:04:50 -0400

    upload de archivo usando api cursonet 2.0

--- admin/recursos/addfile.php
+++ admin/recursos/addfile.php
@@ -2,106 +2,102 @@
 $profile = 'admin'; /////////////// perfil requerido
 include("../../config/setup.php"); ////////setup
 include("../../class/clases.php"); ////////clase
-include ("../../config/lang/{$_SESSION['LENGUAJE']}");////lenguaje
+include("../../config/lang/{$_SESSION['LENGUAJE']}");////lenguaje
 
 require_once("menu.php"); ////////menu
 $menu = new menu($menu_struct);
 
-
- $crear = new tools("db");
-
- 
-   if(isset($_FILES['archivo'])){
-  
-  				 $path = "../../".$ADMINPATH.'archivos/'.$_FILES['archivo']['name']; //////////subiendo a la  carpeta por defecto
-				 if($crear->upload_file($_FILES['archivo'],$path,$TMAX,false,true)){
-				 				 
-				 				 $tama =  bcdiv($_FILES['archivo']['size'],1048576,2); ///tamano en mb
-				 
-				 				 
-								 $valores[0] = 0;
-								 $valores[1] = date("Y-m-d H:i:s");
-								 $valores[2] = $tama.' MB';
-								 $valores[3] = $_FILES['archivo']['name'];
-								 $valores[4] = 'admin';
-								 $valores[5] = $_SESSION['USERID'];
-								 $valores[6] = $_POST['desc'];
-								 
-								 $crear->insertar2("tbl_recurso","tipo, fecha, size, dir, add_by, persona,descripcion",$valores);
-								 $crear->javaviso(LANG_cambios,"index.php");
-				 
-				 }
-				 
-
-	 }
-
 ?>
+<!DOCTYPE html>
 <html>
-<head> <meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="../../css/style_back.css">
-<script type="text/javascript" src="../../js/dyntar.js"></script>
-	<script language="JavaScript" type="text/javascript">
-	function validar(){
-	
-	 if(document.form1.archivo.value==''){
-	 
-	 alert('<?=LANG_content_error ?>');
-	 document.form1.nombre.focus();
-	 
-	 return false;
-	 
-	 }
-	 	 
-	 return true;
-	
-	}
-	</script>
-
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../../css/style_back.css">
+    <script type="text/javascript" src="../../js/dyntar.js"></script>
+    <script type="text/javascript" src="../../js/jquery/jquery-3.3.1.min.js"></script>
 </head>
 
 <body>
 <table width="96%" border="0" align="center" cellpadding="0" cellspacing="0">
-  <tr>
-    <td height="26" valign="top"><?php echo $menu->nombre; ?></td>
-  </tr>
-  <tr>
-    <td><?php $menu->mostrar(0); ?></td>
-  </tr>
-  <tr>
-    <td>
-	
-	<table style="border-right:#000000 solid 1px; border-left:#000000 solid 1px; border-bottom:#000000 solid 1px;" width="100%" border="0" cellspacing="0" cellpadding="0">
-      <tr>
-        <td><form action="addfile.php" method="post" enctype="multipart/form-data" name="form1" onSubmit="return validar();">
-  <table width="100%" border="0" cellspacing="4" cellpadding="3">
-  <tr>
-  <td colspan="2"><span class="style3"><?php echo LANG_content_upload2; ?></span></td>
-</tr>
-  <tr>
-  <td width="19%" class="style3"><?php echo LANG_content_upload; ?></td>
-  <td width="81%"><input name="archivo" type="file" id="archivo" size="40"></td>
-</tr>
-  <tr>
-    <td class="style3"><?php echo LANG_group_desc; ?></td>
-    <td><textarea name="desc" cols="60" rows="4" class="style1" id="desc"></textarea></td>
-  </tr>
-  <tr>
-  <td colspan="2">
-    
-    <input type="button" name="Submit2"  value="<?=LANG_back?>" onClick="location.replace('index.php')">
-    <input type="submit" name="Submit" value="<?=LANG_save?>">
-  </td>  
+    <tr>
+        <td height="26" valign="top"><?php echo $menu->nombre; ?></td>
+    </tr>
+    <tr>
+        <td><?php $menu->mostrar(0); ?></td>
+    </tr>
+    <tr>
+        <td>
+            <table style="border-right:#000000 solid 1px; border-left:#000000 solid 1px; border-bottom:#000000 solid 1px;"
+                   width="100%" border="0" cellspacing="0" cellpadding="0">
+                <tr>
+                    <td>
+                        <form action="" method="post" enctype="multipart/form-data" id="form1" name="form1">
+                            <table width="100%" border="0" cellspacing="4" cellpadding="3">
+                                <tr>
+                                    <td colspan="2"><span class="style3"><?php echo LANG_content_upload2; ?></span></td>
+                                </tr>
+                                <tr>
+                                    <td width="19%" class="style3"><?php echo LANG_content_upload; ?></td>
+                                    <td width="81%"><input name="file" type="file" id="file" size="40"></td>
+                                </tr>
+                                <tr>
+                                    <td class="style3"><?php echo LANG_group_desc; ?></td>
+                                    <td><textarea name="descripcion" cols="60" rows="4" class="style1"
+                                                  id="descripcion"></textarea>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <input id="persona" name="persona" value="<?= $_SESSION['USERID'] ?>"
+                                               type="hidden">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2">
+
+                                        <input type="button" name="Submit2" value="<?= LANG_back ?>"
+                                               onClick="location.replace('index.php')">
+                                        <input id="upload" type="submit"  name="upload" value="<?= LANG_save ?>">
+                                    </td>
+                                </tr>
+                            </table>
+                        </form>
+                    </td>
+                </tr>
+            </table>
+        </td>
     </tr>
 </table>
-</form></td>
-      </tr>
-    </table>	</td>
-  </tr>
-</table>
 </body>
+
+<script>
+    $("#form1").submit(function (eve) {
+
+        eve.preventDefault();
+        var file_data = $("#file").prop("files")[0];   // Getting the properties of file from file field
+        var form_data = new FormData();                  // Creating object of FormData class
+        form_data.append("file", file_data)              // Appending parameter named file with properties of file_field to form_data
+        form_data.append("persona", $("#persona").val())
+        form_data.append("descripcion", $("#descripcion").val())
+        var url = 'http://zserver/cursonet2/api/admin/class/file';
+        $.ajax({
+            url: url,
+            type: 'post',
+            data: form_data,
+            cache       : false,
+            contentType : false,
+            processData : false,
+            success: function (data) {
+                alert('<?=LANG_cambios ?>');
+                location.replace('index.php');
+            },
+            error: function (error) {
+                var msg = error.responseJSON.message;
+                alert(msg);
+            }
+        });
+    })
+</script>
+
+
 </html>
-<?php 
-
- $crear->cerrar();
-
-?>
