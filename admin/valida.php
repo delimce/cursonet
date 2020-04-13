b3f8fad8613389c08a5ece89b44a80cfc90107fa
ldelima <leduardo.delima@modis.com>
2020-04-13 17:44:46 +0200

    Corrigiendo bug de validacion de login en admin, cambio en la query

--- admin/valida.php
+++ admin/valida.php
@@ -10,20 +10,16 @@ if (isset($_POST['user'])) {
     $curso2 = formulario::getvar("curso", $_POST);
     $pass1 = formulario::getvar("pass", $_POST);
 
-   $sql = "SELECT
+    $sql = "SELECT
 	(select lenguaje from tbl_setup) as lenguaje,
     ifnull(group_concat(ac.curso_id),'') as cursos,
 	a.id,
 	CONCAT(nombre, ' ', apellido) AS nombre,
 	a.es_admin,
-	date_format(
-		max(l.fecha_in),
-		'%d/%m/%y %h:%i %p'
-	) AS acc
-        FROM
-        tbl_admin AS a 
-        LEFT JOIN tbl_log_admin AS l ON l.admin_id = a.id
-        LEFT JOIN tbl_admin_curso as ac ON a.id = ac.admin_id
+	( SELECT ifnull(date_format( max( l.fecha_in ), '%d/%m/%y %h:%i %p' ),'')  FROM tbl_log_admin l WHERE l.admin_id = a.id ) AS acc 
+    FROM
+	tbl_admin AS a
+	LEFT JOIN tbl_admin_curso AS ac ON a.id = ac.admin_id 
         WHERE
         a.`user` = '$usuario' AND
         a.pass = MD5('$pass1') GROUP BY a.id";
@@ -33,10 +29,10 @@ if (isset($_POST['user'])) {
 
     if ($db->getNumRows() > 0) {
 
-        $loscursos = ($db->getField("cursos") != "")? explode(',', $db->getField("cursos")):[];
+        $loscursos = ($db->getField("cursos") != "") ? explode(',', $db->getField("cursos")) : [];
         $_SESSION['LENGUAJE'] = $db->getField("lenguaje"); //lenguaje
 
-        if (($db->getField("es_admin") == 1) or ( in_array($curso2, $loscursos))) {
+        if (($db->getField("es_admin") == 1) or (in_array($curso2, $loscursos))) {
 
             $_SESSION['PROFILE'] = 'admin'; ///perfil de ingreso
             $_SESSION['USERID'] = $db->getField("id");
