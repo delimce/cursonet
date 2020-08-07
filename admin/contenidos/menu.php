723650e8100a8b77fa9bf595272fd101af87c7de
ldelima <delimce@gmail.com>
2020-08-07 11:22:50 +0200

    corrigiendo tiulos en minusculas

--- admin/contenidos/menu.php
+++ admin/contenidos/menu.php
@@ -10,7 +10,7 @@ require_once("../../class/menu.php"); ////////clase
 	   "b_color" => "#000000", /////numero de la columna por la cual se desea sacar un total
 	   "b_color_un" => "#B1C4CF", /////numero de la columna por la cual se desea sacar un total
 	   "b_color_se" => "#FFFFFF", /////numero de la columna por la cual se desea sacar un total
-       "items" =>  array( 0 => "mostrar",1 => "crear", 2 => "Agregar Recursos"),
+       "items" =>  array( 0 => "mostrar temas",1 => "Crear tema", 2 => "Asignar recursos"),
 	   "url"  => array(0 => "index.php", 1 => "crear.php", 2 => "recursos.php"),
 	   "ancho_i"  => array(0 => "33%", 1 => "33%", 2 => "*")
           );
